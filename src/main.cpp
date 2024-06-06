a0be6f5b94c93891f947629e506a7c7b99bb3990
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-06 18:40:31 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
 const char* wav_path = "./wav/brk_upfront amen_1 bar_158 bpm.wav";
 
+typedef unsigned char Byte;
+typedef std::array<Byte, 4> FourCC;
+
+static bool fourcc_eq(FourCC four_cc, const char* pattern)
+{
+    for (size_t i = 0; i < 4 && pattern[i]; i++)
+    {
+        if (four_cc[i] != pattern[i])
+            return false;
+    }
+    return true;
+}
+
+static FourCC read_four_cc(const std::vector<Byte>& bytes, size_t& index)
+{
+    assert(index + 4 < bytes.size());
+
+    FourCC out;
+    for (size_t i = 0; i < 4; i++, index++)
+    {
+        out[i] = bytes[index];
+    }
+    return out;
+}
+
 int main()
 {
     std::ifstream input(wav_path, std::ios::binary);
-    std::vector<unsigned char> bytes(std::istreambuf_iterator<char>(input), {});
-    std::cout << bytes.size() << " bytes\n";
+    std::vector<Byte> bytes(std::istreambuf_iterator<char>(input), {});
+
+    size_t index = 0;
+
+    FourCC wave_tag = read_four_cc(bytes, index);
+    assert(fourcc_eq(wave_tag, "WAVE"));
+    assert(index == 4);
 }
