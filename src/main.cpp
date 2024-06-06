014bbbad3425d6cdf22d1b68b63335b66e7df615
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-06 18:28:39 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-#include "cool_ints.h"
+const char* wav_path = "../wav/brk_upfront amen_1 bar_158 bpm.wav";
 
 int main()
 {
-	std::cout << "hi\n";
+    std::ifstream input(wav_path, std::ios::binary);
+    std::vector<unsigned char> bytes(std::istreambuf_iterator<char>(input), {});
+    std::cout << bytes.size() << " bytes\n";
 }
