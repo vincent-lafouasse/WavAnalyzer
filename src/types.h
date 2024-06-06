ad65cfc622e3ed7efbd2293f1b609b0588be65f3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-06 21:04:27 +0200

    sketch Int24 class

--- src/types.h
+++ src/types.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <vector>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -13,3 +15,14 @@ typedef int32_t i32;
 typedef int64_t i64;
 
 typedef unsigned char Byte;
+
+class Int24
+{
+   public:
+    static Int24 read_from_bytes(const std::vector<Byte>& bytes, size_t index);
+    i64 to_i64() const;
+    double to_double() const;
+
+   private:
+    Byte internals[3];
+};
