572c1a6d9af355a7d2af08b933a4f1cb11f8f0d4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-06 20:02:02 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,38 @@
 #include "read.h"
 #include "types.h"
 
+enum class SampleFormat
+{
+    f32,
+    f64,
+    u8,
+    i16,
+    i32,
+    i64,
+};
+
+SampleFormat bit_depth_to_format(u16 bit_depth)
+{
+    switch (bit_depth)
+    {
+        case 8:
+            return SampleFormat::u8;
+        case 16:
+            return SampleFormat::i16;
+        case 32:
+            return SampleFormat::i32;
+        default:
+            std::cout << "unrecognised bit depth " << bit_depth << '\n';
+            exit(1);
+    }
+}
+
+struct WavMetadata
+{
+    u16 n_channels;
+    u32 sample_rate;
+};
+
 const char* wav_path = "./wav/brk_upfront amen_1 bar_158 bpm.wav";
 
 static void skip_chunk(const std::vector<Byte>& bytes, size_t& index)
@@ -21,6 +53,8 @@ int main()
     std::vector<Byte> bytes(std::istreambuf_iterator<char>(input), {});
 
     size_t index = 0;
+
+    WavMetadata metadata;
 
     // riff chunk
     FourCC riff_tag = read_four_cc(bytes, index);
@@ -43,9 +77,9 @@ int main()
     constexpr u16 uncompressed_pcm = 1;
     assert(read_u16(bytes, index) == uncompressed_pcm);
 
-    u16 n_channels = read_u16(bytes, index);
-    std::cout << n_channels << " channels\n";
+    metadata.n_channels = read_u16(bytes, index);
+    std::cout << metadata.n_channels << " channels\n";
 
-    u32 sample_rate = read_u32(bytes, index);
-    std::cout << "sample rate: " << sample_rate << '\n';
+    metadata.sample_rate = read_u32(bytes, index);
+    std::cout << "sample rate: " << metadata.sample_rate << '\n';
 }
