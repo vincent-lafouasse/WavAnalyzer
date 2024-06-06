0122037acbebfd95e251c177f0f00159984b0de3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-06 18:53:12 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 
+#include "cool_ints.h"
+
 const char* wav_path = "./wav/brk_upfront amen_1 bar_158 bpm.wav";
 
 typedef unsigned char Byte;
@@ -16,6 +18,19 @@ static bool fourcc_eq(FourCC four_cc, const char* pattern)
             return false;
     }
     return true;
+}
+
+static u32 read_u32(const std::vector<Byte>& bytes, size_t& index)
+{
+    assert(index + 4 < bytes.size());
+
+    u32 out = 0;
+    for (size_t _ = 0; _ < 4; _++, index++)
+    {
+        out = (out << 8) + static_cast<u32>(bytes[index]);
+    }
+    return out;
+
 }
 
 static FourCC read_four_cc(const std::vector<Byte>& bytes, size_t& index)
@@ -40,4 +55,9 @@ int main()
     FourCC wave_tag = read_four_cc(bytes, index);
     assert(fourcc_eq(wave_tag, "RIFF"));
     assert(index == 4);
+
+    u32 file_size = read_u32(bytes, index) + 8;
+    assert(index ==  8);
+    std::cout << file_size / 64 << std::endl;
+    std::cout << bytes.size() << std::endl;
 }
