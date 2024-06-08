f33b43c1adc15dffe03c797b655ba5e89e6a1657
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-08 17:06:43 +0200

    implement basic failing tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,10 +1,37 @@
 #include "test.h"
 
+#include <cassert>
 #include <cstdlib>
 #include <iostream>
+
+#include "types.h"
+
+static void test_i24();
 
 void run_tests_and_exit()
 {
     std::cout << "Running tests\n";
+    test_i24();
     std::exit(EXIT_SUCCESS);
+}
+
+static void test_i24_positive();
+
+static void test_i24()
+{
+    test_i24_positive();
+}
+
+static void test_i24_positive()
+{
+    Int24 i;
+
+    i = {{0, 0, 0}};
+    assert(i.to_i32() == 0);
+
+    i = {{0x01, 0, 0}};
+    assert(i.to_i32() == 1);
+
+    i = {{0x10, 0, 0}};
+    assert(i.to_i32() == 16);
 }
