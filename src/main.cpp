647d07b303d5e025236127ff477e2d9dd99cf62f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-09 00:51:48 +0200

    sketch spectrogram parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
-#include "OutputStream/OutputStream.h"
 #include "Track/Track.h"
 #include "portaudio.h"
 
-#include <thread>
+#include <iostream>
 
 const char* wav_path = "./wav/brk_upfront amen_1 bar_158 bpm.wav";
-constexpr size_t sample_rate = 48000;
-constexpr size_t buffer_size = 512;
-constexpr size_t n_channels = 1;
 
 struct PlaybackData
 {
@@ -46,21 +42,38 @@ int track_duration_ms(const Track& track)
                      static_cast<float>(track.metadata.sample_rate);
 }
 
+struct SpectrogramParameters
+{
+    u32 frequency_resolution() const;
+    u32 window_duration_ms() const;
+
+    u32 sample_rate;
+    u32 window_size;
+    u32 time_increment;
+};
+
+u32 SpectrogramParameters::frequency_resolution() const
+{
+    return sample_rate / window_size;
+}
+
+u32 SpectrogramParameters::window_duration_ms() const
+{
+    return 1000 * window_size / static_cast<float>(sample_rate);
+}
+
 int main()
 {
     Track track = Track::from_wav(wav_path);
 
-    constexpr OutputStreamConfig cfg = {sample_rate, buffer_size, n_channels};
-    OutputStream stream(cfg);
+    SpectrogramParameters spectrogram_parameters;
+    spectrogram_parameters.sample_rate = track.metadata.sample_rate;
+    spectrogram_parameters.window_size = 64;
 
-    PlaybackData data(track.left);
-    stream.open(&playback, &data);
-    stream.start();
-
-    auto capture_duration = std::chrono::milliseconds(track_duration_ms(track));
-    std::this_thread::sleep_for(capture_duration);
-
-    stream.stop();
+    std::cout << "frequency resolution "
+              << spectrogram_parameters.frequency_resolution() << '\n';
+    std::cout << "window duration "
+              << spectrogram_parameters.window_duration_ms() << " ms\n";
 
     return EXIT_SUCCESS;
 }
