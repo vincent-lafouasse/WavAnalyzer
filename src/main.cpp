96e4de93fa1372fb94a1413a42b1fc9176a76a62
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-08 23:28:36 +0200

    successfully playback audio

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,65 @@
 #include "Track/Track.h"
 #include "portaudio.h"
 
-#include <iostream>
 #include <thread>
 
-const char* wav_path = "./wav/hat_short.wav";
+const char* wav_path = "./wav/brk_upfront amen_1 bar_158 bpm.wav";
 constexpr size_t sample_rate = 48000;
 constexpr size_t buffer_size = 512;
 constexpr size_t n_channels = 1;
 
-int do_nothing_callback(const void* input_buffer,
-                        void* output_buffer,
-                        unsigned long buffer_size,
-                        const PaStreamCallbackTimeInfo* time_info,
-                        PaStreamCallbackFlags status_flags,
-                        void* user_data);
+struct PlaybackData
+{
+    PlaybackData(std::vector<float>& samples) : samples(samples), index(0) {}
+    std::vector<float>& samples;
+    size_t index;
+};
+
+int playback(const void* input_buffer,
+             void* output_buffer,
+             unsigned long buffer_size,
+             const PaStreamCallbackTimeInfo* time_info,
+             PaStreamCallbackFlags status_flags,
+             void* user_data)
+{
+    (void)input_buffer;
+    (void)time_info;
+    (void)status_flags;
+
+    PlaybackData* data = static_cast<PlaybackData*>(user_data);
+    float* output = static_cast<float*>(output_buffer);
+
+    for (size_t i = 0; i < buffer_size; i++)
+    {
+        output[i] =
+            (i < data->samples.size()) ? data->samples[data->index] : 0.0;
+        data->index += 1;
+    }
+
+    return 0;
+}
+
+int track_duration_ms(const Track& track)
+{
+    return 250 + 1000 * static_cast<float>(track.left.size()) /
+                     static_cast<float>(track.metadata.sample_rate);
+}
 
 int main()
 {
     Track track = Track::from_wav(wav_path);
 
     constexpr OutputStreamConfig cfg = {sample_rate, buffer_size, n_channels};
-
     OutputStream stream(cfg);
-    stream.open(&do_nothing_callback, NULL);
+
+    PlaybackData data(track.left);
+    stream.open(&playback, &data);
     stream.start();
 
-    auto capture_duration = std::chrono::seconds(10);
+    auto capture_duration = std::chrono::milliseconds(track_duration_ms(track));
     std::this_thread::sleep_for(capture_duration);
 
     stream.stop();
 
     return EXIT_SUCCESS;
-}
-
-int do_nothing_callback(const void* input_buffer,
-                        void* output_buffer,
-                        unsigned long buffer_size,
-                        const PaStreamCallbackTimeInfo* time_info,
-                        PaStreamCallbackFlags status_flags,
-                        void* user_data)
-{
-    (void)input_buffer;
-    (void)output_buffer;
-    (void)buffer_size;
-    (void)time_info;
-    (void)status_flags;
-    (void)user_data;
-    std::cout << 'a';
-    std::cout.flush();
-    return 0;
 }
