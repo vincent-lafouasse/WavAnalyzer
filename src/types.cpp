bfde2f47aebaf433f4dc79d04f6505cf66c6b59f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-08 17:09:50 +0200

    implement Int24::to_i32 for positive numbers

--- src/types.cpp
+++ src/types.cpp
@@ -2,5 +2,10 @@
 
 i32 Int24::to_i32() const
 {
-    return 0;
+    u32 out = 0;
+
+    out = little_endian_bytes[0] | little_endian_bytes[1] << 8 |
+          little_endian_bytes[2] << 16;
+
+    return *((i32*)&out);
 }
