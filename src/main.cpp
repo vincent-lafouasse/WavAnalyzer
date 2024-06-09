2db3f5ac3f490d146e0d841dec451bbd85d9827a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-09 16:17:13 +0200

    naive dft that takes way too much time

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "Track/Track.h"
 
+#include <complex>
 #include <iostream>
-#include <numeric>
+
+typedef std::complex<float> Complex;
 
 const char* wav_path = "./wav/nice_chord.wav";
 
@@ -25,12 +27,31 @@ void log(T object, const char* name)
 int main()
 {
     Track track = Track::from_wav(wav_path);
-    track.play_mono_blocking();
     std::vector<float> signal = track.left;
 
-    float total_energy = std::inner_product(signal.cbegin(), signal.cend(),
-                                            signal.cbegin(), 0.0);
-    log(total_energy, "total energy");
+    const float two_pi = 2 * std::acos(-1);
+    const Complex imaginary_unit(0.0, 1.0);
+    const size_t N = signal.size();
+
+    std::vector<Complex> dft;
+    for (size_t k = 0; k < N; k++)  // a naive n^2 DFT
+    {
+        Complex X_k = 0;
+
+        for (size_t n = 0; n < N; n++)
+        {
+            X_k += signal[n] *
+                   std::exp(-imaginary_unit * two_pi *
+                            static_cast<float>(k * n) / static_cast<float>(N));
+        }
+
+        dft.push_back(X_k);
+        float completion = 100.0 * k / static_cast<float>(N);
+        std::cout << completion << '\n';
+    }
+
+    log(signal.size(), "input size");
+    log(dft.size(), "dft size");
 
     return EXIT_SUCCESS;
 }
