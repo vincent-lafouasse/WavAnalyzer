4120724d65427762f02d8ba1f471a7b24d687325
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-09 17:23:51 +0200

    add python csv plotting script

---
+++ tools/plot_csv.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    if (len(sys.argv) != 2):
+        print(f"Usage python3 plot_csv.py foo.csv")
+        exit(1)
+
+    csv_path = sys.argv[1]
+    print(f"Plotting {csv_path}")
+
+    values = np.genfromtxt(csv_path, delimiter=',')
+    indices = np.arange(0, len(values))
+
+    fig, ax = plt.subplots()
+    ax.plot(indices, values)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
