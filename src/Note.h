1a60e42318b3202c1e3436338414b33c3005c099
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-10 14:28:00 +0200

    create Note module

---
+++ src/Note.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Track/types.h"
+
+#include <string>
+
+struct Note
+{
+    Note(u32 midi_note) : midi_note(midi_note) {}
+    float frequency();
+    size_t corresponding_frequency_bucket(u32 dft_size, u32 sample_rate);
+    std::string name();
+
+    size_t midi_note;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "Note.h"
 #include "Track/Track.h"
 #include "log.hpp"
 
@@ -12,61 +13,6 @@ const char* wav_path = "./wav/simple_chord.wav";
 [[maybe_unused]] const float pi = std::acos(-1);
 [[maybe_unused]] const Complex imaginary_unit(0.0, 1.0);
 [[maybe_unused]] const Complex two_i_pi(0.0, 2 * pi);
-
-struct Note
-{
-    Note(u32 midi_note) : midi_note(midi_note) {}
-    float frequency()
-    {
-        const i32 offset_from_a4 = midi_note - 69;
-        return 440.0 * std::exp2(offset_from_a4 / 12.0);
-    }
-    size_t corresponding_frequency_bucket(u32 dft_size, u32 sample_rate)
-    {
-        const float frequency_unit =
-            static_cast<float>(sample_rate) / static_cast<float>(dft_size);
-        return std::round(this->frequency() / frequency_unit);
-    }
-    std::string name()
-    {
-        int octave = (midi_note / 12) - 1;
-        int notename = midi_note % 12;
-        switch (notename)
-        {
-            case 0:
-                return "C" + std::to_string(octave);
-            case 1:
-                return "C#/Db" + std::to_string(octave);
-            case 2:
-                return "D" + std::to_string(octave);
-            case 3:
-                return "Eb/D#" + std::to_string(octave);
-            case 4:
-                return "E" + std::to_string(octave);
-            case 5:
-                return "F" + std::to_string(octave);
-            case 6:
-                return "F#/Gb" + std::to_string(octave);
-            case 7:
-                return "G" + std::to_string(octave);
-            case 8:
-                return "Ab/G#" + std::to_string(octave);
-            case 9:
-                return "A" + std::to_string(octave);
-            case 10:
-                return "Bb/A#" + std::to_string(octave);
-            case 11:
-                return "B" + std::to_string(octave);
-            default:
-                std::cout << "unreachable";
-                exit(1);
-        }
-    }
-    size_t midi_note;
-};
-
-template <typename T>
-void write_to_csv(const std::vector<T>& data);
 
 // a nasty FFT with a bunch of allocation in recursion
 std::vector<Complex> FFT_out_of_place(std::vector<Complex> input)
