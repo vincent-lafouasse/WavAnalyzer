f4e40ec73fae50efff4cd515bd62e6f345253726
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-15 01:18:34 +0200

    make input complex

--- aux/blackboard/main.cpp
+++ aux/blackboard/main.cpp
@@ -2,25 +2,33 @@
 #include <complex>
 #include <iostream>
 
+namespace Constants {
+[[maybe_unused]] static constexpr float pi = 3.14159265359f;
+[[maybe_unused]] static constexpr Complex j = {0, 1};
+[[maybe_unused]] static constexpr Complex twoPiJ = 2.0f * pi * j;
+}  // namespace Constants
+
 using Complex = std::complex<float>;
 
 using usize = std::size_t;
 
 static constexpr usize bufferSize = 8;
-static constexpr float pi = 3.14159265359f;
 
 float t(usize i) {
     return static_cast<float>(i) / bufferSize;
 }
 
 int main() {
-    std::array<float, bufferSize> input;
+    std::array<float, bufferSize> realInput;
     for (usize i = 0; i < bufferSize; ++i) {
-        input[i] = std::sin(2.0f * pi * t(i));
+        realInput[i] = std::sin(2.0f * Constants::pi * t(i));
     }
 
-    for (float x : input) {
-        std::cout << x << ',';
+    std::array<Complex, bufferSize> input;
+    std::transform(realInput.cbegin(), realInput.cend(), input.begin(),
+                   [](float f) { return f; });
+
+    for (Complex x : input) {
+        std::cout << x << '\n';
     }
-    std::cout << '\n';
 }
