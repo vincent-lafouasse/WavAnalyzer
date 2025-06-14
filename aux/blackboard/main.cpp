966963527802297f54be200d56835d9685e7b6c3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-15 01:24:06 +0200

    naive dft

--- aux/blackboard/main.cpp
+++ aux/blackboard/main.cpp
@@ -2,20 +2,27 @@
 #include <complex>
 #include <iostream>
 
-namespace Constants {
-[[maybe_unused]] static constexpr float pi = 3.14159265359f;
-[[maybe_unused]] static constexpr Complex j = {0, 1};
-[[maybe_unused]] static constexpr Complex twoPiJ = 2.0f * pi * j;
-}  // namespace Constants
-
 using Complex = std::complex<float>;
 
 using usize = std::size_t;
 
 static constexpr usize bufferSize = 8;
 
+
+namespace Constants {
+[[maybe_unused]] static constexpr float pi = 3.14159265359f;
+[[maybe_unused]] static constexpr Complex j = {0, 1};
+[[maybe_unused]] static constexpr Complex twoPiJ = 2.0f * pi * j;
+}  // namespace Constants
+
 float t(usize i) {
     return static_cast<float>(i) / bufferSize;
+}
+
+Complex exponent(usize n, usize k, usize N) {
+    const Complex temp = static_cast<float>(n * k) / N;
+
+    return std::exp(-Constants::twoPiJ * temp);
 }
 
 int main() {
@@ -28,7 +35,14 @@ int main() {
     std::transform(realInput.cbegin(), realInput.cend(), input.begin(),
                    [](float f) { return f; });
 
-    for (Complex x : input) {
-        std::cout << x << '\n';
+    std::array<Complex, bufferSize> complexFourier{};
+    for (usize i = 0; i < bufferSize; ++i) {
+        for (usize j = 0; j < bufferSize; ++j) {
+            complexFourier[i] += input[j] * exponent(i, j, bufferSize);
+        }
+    }
+
+    for (Complex z: complexFourier) {
+        std::cout << std::norm(z) << '\n';
     }
 }
