8234dd20dd9a8d42d131fe3a2e0b14b8fe620838
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-15 13:01:36 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <complex>
 #include <iostream>
-#include <numeric>
 
 #include "colors/ColorMap.hpp"
 #include "colors/Rgb.hpp"
 
 #include <raylib.h>
 
-typedef std::complex<float> Complex;
+typedef float Float;
+typedef std::complex<Float> Complex;
 
 namespace Constants {
-[[maybe_unused]] const float pi = std::acos(-1);
-[[maybe_unused]] const Complex j(0.0, 1.0);
+[[maybe_unused]] static constexpr float fPi = M_PI;
+[[maybe_unused]] static constexpr Complex cPi = fPi;
+[[maybe_unused]] static constexpr float fTau = 2.0f * fPi;
+[[maybe_unused]] static constexpr Complex cTau = fTau;
+[[maybe_unused]] static constexpr Complex j(0.0, 1.0);
 }  // namespace Constants
+
+Complex nthRootOfUnity(usize n) {
+    assert(n > 0);
+
+    namespace C = Constants;
+
+    return std::exp(C::cTau * C::j / static_cast<Complex>(n));
+}
+
+Complex cPow(Complex z, usize n) {
+    if (n == 0) {
+        return 1.0f;
+    } else if (n == 1) {
+        return z;
+    }
+
+    const Complex halfTerm = cPow(z, n / 2);
+
+    if (n % 2 == 0) {
+        return halfTerm * halfTerm;
+    } else {
+        return z * halfTerm * halfTerm;
+    }
+}
 
 namespace OutOfPlace {
 std::vector<Complex> fftRecurse(const Complex* input, usize size, usize step) {
@@ -26,13 +54,12 @@ std::vector<Complex> fftRecurse(const Complex* input, usize size, usize step) {
     const std::vector<Complex> odds =
         fftRecurse(input + step, size / 2, 2 * step);
 
+    const Complex omega = nthRootOfUnity(size);
+
     std::vector<Complex> out(size, 0);
     for (usize i = 0; i < size / 2; ++i) {
-        const Complex e_i = input[i * step];
-        const Complex o_i =
-            input[step * (i + size / 2)] *
-            std::exp(-Constants::j *
-                     static_cast<Complex>(2.0 * Constants::pi * i / size));
+        const Complex e_i = evens[i];
+        const Complex o_i = cPow(omega, i) * odds[i];
 
         out[i] = e_i + o_i;
         out[i + size / 2] = e_i - o_i;
@@ -107,34 +134,30 @@ int main(int ac, char** av) {
     ColorMap cmap = ColorMap::Viridis();
 
     std::array<float, bufferSize> localBuffer{};
-    std::vector<float> fft(0, bufferSize);
-
-    bool didTheThing = false;
+    std::vector<float> fft(bufferSize, 0);
 
     while (!WindowShouldClose()) {
         UpdateMusicStream(music);
 
-        if (bufferIsReady.load() && !didTheThing) {
+        if (bufferIsReady.load()) {
             localBuffer = globalBuffer;
             bufferIsReady.store(false);
             fft = OutOfPlace::fft(&localBuffer[0], bufferSize);
-            for (float f : fft) {
-                std::cout << f << ",";
-            }
-            std::cout << '\n';
-            didTheThing = true;
         }
 
         auto floatToHeight = [&](float f) {
-            return static_cast<int>((1.0f - f) * screenHeight);
+            return static_cast<int>(f * screenHeight);
         };
 
         BeginDrawing();
         ClearBackground(catpuccin::DarkGray.opaque());
-        if (didTheThing) {
-            for (usize i = 0; i < bufferSize; ++i) {
-                DrawPixel(i, floatToHeight(fft[i]), catpuccin::Pink.opaque());
-            }
+        for (usize i = 0; i < bufferSize / 2; ++i) {
+            static constexpr int barWidth = 4;
+            const Color c =
+                cmap.get(2.0f * static_cast<float>(i) / bufferSize).opaque();
+            const int h = floatToHeight(fft[i]);
+
+            DrawRectangle(i * barWidth, screenHeight - h, barWidth, h, c);
         }
         DrawFPS(0, 0);
         EndDrawing();
