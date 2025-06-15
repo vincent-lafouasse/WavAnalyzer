503561693b0e81068fadb0a1a43341a0236c8c89
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-15 13:10:55 +0200

    format

--- src/fft.cpp
+++ src/fft.cpp
@@ -4,7 +4,6 @@
 
 #include "constants.h"
 
-
 Complex nthRootOfUnity(usize n) {
     assert(n > 0);
 
--- src/types.h
+++ src/types.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <array>
-#include <cstdint>
 #include <complex>
+#include <cstdint>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
