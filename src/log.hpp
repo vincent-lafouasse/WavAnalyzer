3cde5439857dd9facd8e1350560f5ca2990cc16f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-08-31 19:22:05 +0200

    format

--- src/log.hpp
+++ src/log.hpp
@@ -1,24 +1,21 @@
 #pragma once
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 template <typename T>
 [[maybe_unused]]
-void log(T object, const char* name)
-{
+void log(T object, const char* name) {
     std::cout << name << " : " << object << '\n';
     std::cout.flush();
 }
 
 template <typename T>
 [[maybe_unused]]
-void write_to_csv(const std::vector<T>& data, const std::string& filename)
-{
+void write_to_csv(const std::vector<T>& data, const std::string& filename) {
     std::ofstream csv;
     csv.open(filename);
-    for (const T& cell : data)
-    {
+    for (const T& cell : data) {
         csv << cell << ",";
     }
     csv << std::endl;
